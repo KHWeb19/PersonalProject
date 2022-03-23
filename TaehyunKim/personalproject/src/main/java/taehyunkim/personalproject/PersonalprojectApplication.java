784c1fccfcb1806e0fdd6249c4b02c0db73246e5
thkim750 <thkim750@gmail.com>
2022-03-23 20:50:05 +0900

    [김태현]개인프로젝트

---
+++ TaehyunKim/personalproject/src/main/java/taehyunkim/personalproject/PersonalprojectApplication.java
@@ -0,0 +1,13 @@
+package taehyunkim.personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalprojectApplication.class, args);
+	}
+
+}
---
+++ TaehyunKim/personalproject/src/test/java/taehyunkim/personalproject/PersonalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package taehyunkim.personalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
