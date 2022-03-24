c7a7a9eaa4503a7d2c742bcbcec1499ca47480f6
SeohyunNoh <sarahnoh7@naver.com>
2022-03-24 16:16:10 +0900

    [노서현] init project repo

---
+++ SeohyunNoh/frontend_project/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+      <router-link style="text-decoration: none;" :to="{ name: 'Home' }">
+        &nbsp;메인페이지 입장&nbsp;
+      </router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
---
+++ SeohyunNoh/frontend_project/src/views/project/MainPage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-toolbar dense dark>
+            <v-app-bar-nav-icon @click="nav_drawer = !nav_drawer">
+            </v-app-bar-nav-icon>
+            <v-toolbar-title>
+                <span class="font-weight-light">온라인 편집샵</span>
+            </v-toolbar-title>
+            <v-toolbar-items>
+                <v-btn text v-for="link in links" :key="link.icon" :to="link.route">
+                    {{ link.text }}
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+
+        <v-navigation-drawer app v-model="nav_drawer" temporary>
+            <v-list nav dense>
+                <v-list-item v-for="link in links" :key="link.name" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon left>
+                            {{ link.icon }}
+                        </v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            {{ link.text}}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          nav_drawer: false,
+          links: [
+              { icon: 'mdi-home', text: 'Home', name: 'home', route: '/' },
+              { icon: 'mdi-account', text: '로그인', name: 'login', route: '/logInPage' },
+              { icon: 'mdi-cart', text: '장바구니', name: 'cart', route: '/cartPage' }
+          ]  
+        }
+    }
+}
+</script>
