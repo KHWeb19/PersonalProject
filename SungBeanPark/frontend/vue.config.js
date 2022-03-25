a0d0f975bce3cec5b3e43a5c9b7b6f0aa02c9b13
Mong9 <bean326@naver.com>
2022-03-25 09:07:34 +0900

    ✨ Add scss loader

--- SungBeanPark/frontend/vue.config.js
+++ SungBeanPark/frontend/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/scss/abstracts.scss";
+    `,
+      },
+    },
+  },
+}
