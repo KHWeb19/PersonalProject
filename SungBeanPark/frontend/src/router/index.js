2c96c738d3cc17452079ab7818f83a2c800744c0
Mong9 <bean326@naver.com>
2022-03-27 20:28:07 +0900

    💄 Add sidebar style

--- SungBeanPark/frontend/src/router/index.js
+++ SungBeanPark/frontend/src/router/index.js
@@ -1,29 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import SignPage from "../views/SignPage.vue"
+import LoginPage from "../views/LoginPage.vue"
+import PasswordPage from "../views/PasswordPage.vue"
+import IdPage from "../views/IdPage.vue"
+import HeaderPage from "../views/HeaderPage.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/sign",
+    name: "sign",
+    component: SignPage,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: "/login",
+    name: "login",
+    component: LoginPage,
+  },
+  {
+    path: "/password",
+    name: "password",
+    component: PasswordPage,
+  },
+  {
+    path: "/id",
+    name: "id",
+    component: IdPage,
+  },
+  {
+    path: "/header",
+    name: "header",
+    component: HeaderPage,
+  },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
--- SungBeanPark/frontend/src/views/HeaderPage.vue
+++ SungBeanPark/frontend/src/views/HeaderPage.vue
@@ -16,14 +16,16 @@
                   <h2 class="visually-hidden">메뉴</h2>
                   <ul class="gnb-nav-list">
                     <li class="gnb-nav-item"><a href="/">Best</a></li>
-                    <li class="gnb-nav-item"><a href="/">New Release</a></li>
+                    <li class="gnb-nav-item is-active">
+                      <a href="/">New Release</a>
+                    </li>
                     <li class="gnb-nav-item"><a href="/">Men</a></li>
                     <li class="gnb-nav-item"><a href="/">Women</a></li>
                   </ul>
                 </nav>
 
                 <button
-                  class="gnb-icon-button sm-only"
+                  class="gnb-icon-button is-menu sm-only"
                   type="button"
                   aria-label="메뉴 열기 버튼"
                 >
@@ -34,16 +36,12 @@
               <div class="gnb-right">
                 <div class="input-group lg-only">
                   <i class="ic-search" aria-hidden></i>
-                  <input
-                    class="form-input"
-                    type="text"
-                    placeholder="스토어 검색"
-                  />
+                  <input class="form-input" type="text" placeholder="Search" />
                 </div>
 
                 <div class="button-group">
                   <button
-                    class="gnb-search-button gnb-icon-button lg-hidden"
+                    class="gnb-icon-button is-search lg-hidden"
                     type="button"
                     aria-label="검색창 열기 버튼"
                   >
@@ -58,7 +56,7 @@
                     <i class="ic-cart"></i>
                   </a>
 
-                  <nav class="sm-hidden">
+                  <nav class="gnb-text sm-hidden">
                     <a href="">Login</a>
                     <a href="">Support</a>
                   </nav>
@@ -69,6 +67,57 @@
         </div>
       </div>
     </header>
+
+    <!-- <aside class="sidebar">
+      <header class="sidebar-header">
+        <h1 class="logo">
+          <a href="/">
+            <img src="@/assets/images/Logo.svg" alt="나이키" />
+          </a>
+        </h1>
+
+        <nav class="sidebar-nav">
+          <h2 class="visually-hidden">메뉴</h2>
+          <div class="sidebar-user-menu">
+            <ul class="user-menu-list">
+              <li class="user-menu-item"><a href="/">Best</a></li>
+              <li class="user-menu-item"><a href="/">New Release</a></li>
+              <li class="user-menu-item"><a href="/">Men</a></li>
+              <li class="user-menu-item"><a href="/">Women</a></li>
+              <li class="user-menu-item"><a href="/">Login</a></li>
+              <li class="user-menu-item"><a href="/">Cart</a></li>
+              <li class="user-menu-item"><a href="/">Support</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </aside> -->
+    <section class="home-section sm-only">
+      <div class="home-section-content">
+        <p class="home-section-text">
+          <a href="/">회원 가입</a>하고 10% 할인 쿠폰, 무료 배송 등의 혜택을
+          받으세요.
+        </p>
+      </div>
+    </section>
+
+    <div class="swiper">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <figure class="swiper-figure">
+              <div class="swiper-container">
+                <div class="swiper-wrapper">
+                  <a href="/">
+                    <img src="@/assets/images/nike.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </template>
 <script>
@@ -76,4 +125,10 @@ export default {}
 </script>
 <style lang="scss" scoped>
 @import "~@/assets/scss/layout/header";
+@import "~@/assets/scss/components/gnb-nav";
+@import "~@/assets/scss/components/gnb-icon-buttons";
+@import "~@/assets/scss/layout/sidebar";
+@import "~@/assets/scss/components/home-section";
+@import "~@/assets/scss/components/swiper";
+@import "~@/assets/scss/layout/sidebar";
 </style>
