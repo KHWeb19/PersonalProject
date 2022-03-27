f90cdc1c8dc7ecbaa1bab8a3fd63508eb9805b5f
ljieun <wldms8196@gmail.com>
2022-03-27 23:57:02 +0900

    로그인, 회원가입 ui 완료

--- jieunLee/frontend/src/router/index.js
+++ jieunLee/frontend/src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/user/LoginPage'
+import RegisterPage from '../views/user/RegisterPage'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HomeView',
+    component: HomeView
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage
   }
 ]
 
---
+++ jieunLee/frontend/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <router-link style="text-decoration: none;" :to="{name: 'HomeView'}">
+      &nbsp;Home&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{name: 'LoginPage'}">
+      &nbsp;Login&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{name: 'RegisterPage'}">
+      &nbsp;Register&nbsp;
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
