2f452c32defd3048aa2fa14d6d41ed7bfed303cc
sinareum <kuuniin@gmail.com>
2022-03-27 23:12:17 +0900

    [신아름] UI 작업

--- AreumSin/frontend/src/components/CalendarComponents.vue
+++ AreumSin/frontend/src/components/CalendarComponents.vue
@@ -1,14 +1,55 @@
 <template>
-  <v-row>
+  <v-row class="fill-height">
     <v-col>
-      <v-card height="500" outlined> 캘린더 </v-card>
+      <v-sheet height="100"> <!-- 캘린더와 3월 사이 차이 -->
+        <v-toolbar flat="flat" align="center">
+          <v-btn fab="fab" text="text" small="small" color="grey darken=-2" @click="prev">
+            <v-icon small="small">
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }} <!-- 해당 달-->
+          </v-toolbar-title>
+          <v-btn fab="fab" text="text" small="small" color="grey darken=-2" @click="next">
+            <v-icon small="small">
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-toolbar>
+
+      </v-sheet>
+      <v-sheet height="100%">
+        <v-calendar ref="calendar" v-model="focus" color="primary" :type="type">
+        </v-calendar>
+
+      </v-sheet>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: "CalendarComponents"
+  name: "CalendarComponents",
+  data() {
+    return{
+      focus: '',
+      type: 'month',
+    }
+  },
+  methods: {
+    viewDay ({ date }) {
+      this.focus = date
+      this.type = 'day'
+    },
+    prev () {
+      this.$refs.calendar.prev()
+    },
+    next () {
+      this.$refs.calendar.next()
+    },
+
+  }
 }
 </script>
 
