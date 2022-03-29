f94d13ff636aa8987123ba781e2a57a298ff7dd1
dkswnsgur <dkseho2135@naver.com>
2022-03-29 22:22:04 +0900

    [안준혁] init project repo

--- junhurkahn/spring/demo/src/main/java/com/example/demo/repository/jpa/order62/JpaBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository.jpa.order62;
-
-import com.example.demo.entity.jpa.JpaBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaBoardRepository extends JpaRepository<JpaBoard, Long> {
-}
