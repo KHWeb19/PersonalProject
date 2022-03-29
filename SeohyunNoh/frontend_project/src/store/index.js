ea04ce66b802fa422b0bc2c10666b4d24fce9495
SeohyunNoh <sarahnoh7@naver.com>
2022-03-29 23:00:07 +0900

    [노서현] 개인,판매자,관리자 회원가입 db 생성 전

--- SeohyunNoh/frontend_project/src/store/index.js
+++ SeohyunNoh/frontend_project/src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from './states'
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
+  // modules: {
+  // }
+  state,
+  actions,
+  mutations,
+  getters
 })
