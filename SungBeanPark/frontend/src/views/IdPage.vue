38a723a0cf6180bd95ad60308fa803cea1003b80
Mong9 <bean326@naver.com>
2022-03-30 23:31:25 +0900

    ♻️ Refactor Add and Remove

--- SungBeanPark/frontend/src/views/IdPage.vue
+++ SungBeanPark/frontend/src/views/IdPage.vue
@@ -4,62 +4,37 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-4">
-            <header class="visually-hidden">
-              <h1>E-commerce</h1>
+            <header>
+              <h1 class="visually-hidden">Nike</h1>
             </header>
 
-            <div class="id-header">아이디 찾기</div>
-
-            <section>
-              <form action="/" method="POST">
-                <div>
-                  <div class="id-radio">
-                    <input type="radio" />
-                    <label class="id-radio-label" for="">휴대전화</label>
-                  </div>
-                  <div class="id-input">
-                    <div class="id-input-wrap">
+            <div class="id-container">
+              <div class="id-header">
+                <h2>아이디 찾기</h2>
+              </div>
+              <section class="id-section">
+                <div class="id-member">
+                  <form class="id-form">
+                    <div class="id-input-label">
+                      <label for="">가입시 입력하신 휴대폰 번호</label>
+                    </div>
+                    <div class="id-input">
                       <input
                         class="id-input-input"
                         type="text"
-                        placeholder="이름"
-                      />
-                      <button class="id-input-button" type="button"></button>
-                    </div>
-                  </div>
-                  <div class="id-input">
-                    <div class="id-input-wrap">
-                      <input
-                        class="id-input-input"
-                        type="tel"
                         placeholder="휴대전화(-없이)"
-                        maxlength="11"
                       />
                       <button class="id-input-button" type="button">
                         인증 요청
                       </button>
                     </div>
-                  </div>
-                  <div class="id-radio">
-                    <input type="radio" />
-                    <label class="id-radion-label" for="">이메일</label>
-                  </div>
-                  <div class="id-input">
-                    <div class="id-input-wrap">
-                      <input
-                        class="id-input-input"
-                        type="email"
-                        placeholder="이메일"
-                        maxlength="11"
-                      />
-                      <button class="id-input-button" type="button">
-                        인증 요청
-                      </button>
-                    </div>
-                  </div>
+                  </form>
                 </div>
-              </form>
-            </section>
+                <div class="id-button">
+                  <button class="btn-black btn-46" type="button">다음</button>
+                </div>
+              </section>
+            </div>
           </div>
         </div>
       </div>
