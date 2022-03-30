3c299c39b92d4c7cd322b864ea662f2fb20e7444
ljieun <wldms8196@gmail.com>
2022-03-30 22:05:12 +0900

    [이지은] db 스키마 생성

--- jieunLee/backend/src/main/java/com/example/backend/controller/MemberController.java
+++ jieunLee/backend/src/main/java/com/example/backend/controller/MemberController.java
@@ -1,4 +1,4 @@
 package com.example.backend.controller;
 
-public class UserController {
+public class MemberController {
 }
--- jieunLee/backend/src/main/java/com/example/backend/controller/TestController.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend.controller;
-
-public class TestController {
-}
