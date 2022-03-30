c45a8183a6d2dcb1d5535f4c4d986ab187d69b86
HyeonJi-Son <sustka46@gmail.com>
2022-03-31 01:42:47 +0900

    [손현지] layout 작업 중

--- hyeonjiSon/frontend/src/views/HomeView.vue
+++ hyeonjiSon/frontend/src/views/HomeView.vue
@@ -6,7 +6,8 @@
   import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'Home',
+    //eslint-disable-next-line
+    name: 'HomeView',
 
     components: {
       HelloWorld,
@@ -14,19 +15,14 @@
   }
   /*
   export default {
-
 name: 'Discount-a'
-
 }
 
 이런식으로 '-' 이용해서 여러단어로 구성해주거나
 
 export default {
-
 //eslint-disable-next-line
-
 name: 'Discount'
-
 }
 
 이렇게 위해 주석 추가해주면 됩니다.
