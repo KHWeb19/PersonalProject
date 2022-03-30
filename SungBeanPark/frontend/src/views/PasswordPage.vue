6af8c9ab454876a59304e8f1e426049731341aa1
Mong9 <bean326@naver.com>
2022-03-30 23:32:15 +0900

    ♻️ Refactor Add and Remove

--- SungBeanPark/frontend/src/views/PasswordPage.vue
+++ SungBeanPark/frontend/src/views/PasswordPage.vue
@@ -4,28 +4,47 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-4">
-            <header class="visually-hidden">
-              <h1>E-commerce</h1>
+            <header>
+              <h1 class="visually-hidden">Nike</h1>
             </header>
 
-            <div class="password-header">
-              <h2>비밀번호 찾기</h2>
-            </div>
-
-            <div class="password-line"></div>
-
-            <section class="password-section">
-              <div class="password-member">
-                <form class="password-form" action="/" method="POST">
-                  <div class="password-input">
-                    <label for="/">가입한 아이디를 입력해주세요</label>
-                    <input type="text" placeholder="아이디" />
-                  </div>
-
-                  <button class="btn-black btn-46">다음</button>
-                </form>
+            <div class="password-container">
+              <div class="password-header">
+                <h2>비밀번호 찾기</h2>
               </div>
-            </section>
+              <section class="password-section">
+                <div class="password-member">
+                  <form class="password-form">
+                    <div class="password-input-label">
+                      <label for="">비밀번호를 찾으려는 아이디</label>
+                    </div>
+                    <div class="password-input">
+                      <input
+                        class="password-input-input"
+                        type="text"
+                        placeholder="아이디 입력"
+                      />
+                    </div>
+                    <div class="password-input-label">
+                      <label for="">가입시 입력하신 휴대폰 번호</label>
+                    </div>
+                    <div class="password-input">
+                      <input
+                        class="password-input-input"
+                        type="text"
+                        placeholder="휴대전화(-없이)"
+                      />
+                      <button class="password-input-button" type="button">
+                        인증 요청
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="password-button">
+                  <button class="btn-black btn-46" type="button">다음</button>
+                </div>
+              </section>
+            </div>
           </div>
         </div>
       </div>
