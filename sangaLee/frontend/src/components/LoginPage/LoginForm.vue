77310a33922c68487c72001a9110dae181fc9399
asalwaysineedu <lso7182@naver.com>
2022-04-01 16:21:20 +0900

    [이상아] feat: Login UI implementing [FLP]

--- sangaLee/frontend/src/components/LoginPage/LoginForm.vue
+++ sangaLee/frontend/src/components/LoginPage/LoginForm.vue
@@ -3,7 +3,12 @@
     <div class="color"></div>
     <div class="color"></div>
     <div class="color"></div>
-    <div class="box"></div>
+    <div class="box">
+    <div class="square" style="--i:0;"></div>
+    <div class="square" style="--i:1;"></div>
+    <div class="square" style="--i:2;"></div>
+    <div class="square" style="--i:3;"></div>
+    <div class="square" style="--i:4;"></div>
       <div class="container">
         <div class="form">
           <h2>Login Form</h2>
@@ -22,8 +27,8 @@
           </form>
         </div>
       </div>
+    </div>
   </section>
-
 </template>
 
 <script>
