f3513a1b96644be81564c46e0820a00e650c15d6
woosoojong <woo2886@naver.com>
2022-04-01 13:51:36 +0900

    [우수종] 메인페이지 UI[MP]

--- SooJongWoo/frontend/src/views/MainPage.vue
+++ SooJongWoo/frontend/src/views/MainPage.vue
@@ -1,57 +1,31 @@
 <template>
-  <div>
-    <v-toolbar dense dark>
-      <v-toolbar-items>
-        <v-btn text v-for="link in links" :key="link.icon" :to="link.route">
-          {{ link.text }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-navigation-drawer app v-model="nav_drawer" temporary>
-      <v-list nav dense>
-        <v-list-item
-          v-for="link in links"
-          :key="link.name"
-          router
-          :to="link.route"
-        >
-          <v-list-item-action>
-            <v-icon left>
-              {{ link.icon }}
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ link.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </div>
+  <swiper class="swiper">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide>
+    <swiper-slide>Slide 9</swiper-slide>
+    <swiper-slide>Slide 10</swiper-slide>
+  </swiper>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+import VueAweSomeSwiper from "vue-awesome-swiper";
+// eslint-disable-next-line no-undef
+Vue.use(VueAweSomeSwiper);
+
 export default {
-  data() {
-    return {
-      nav_drawer: false,
-      service: {},
-      links: [
-        { icon: "mdi-home", text: "홈로고", name: "home", route: "/" },
-        { icon: "mdi-door", text: "객실안내", name: "room", route: "/" },
-        { icon: "mdi-check", text: "예약안내", name: "check", route: "/" },
-        { icon: "mdi-coffee", text: "부대시설", name: "coffee", route: "/" },
-        { icon: "mdi-notice", text: "공지사항", name: "notice", route: "/" },
-      ],
-    };
+  name: "swiper-example-default",
+  title: "Default",
+  components: {
+    Swiper,
+    SwiperSlide,
   },
-  components: {},
-  methods: {},
-  created: {},
-  computed: {},
 };
 </script>
-
-<style scoped>
-</style>
