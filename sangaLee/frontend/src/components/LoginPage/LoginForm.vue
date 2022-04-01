045565931609384d94bbccea479ab595dd8b5c62
asalwaysineedu <lso7182@naver.com>
2022-04-01 14:44:45 +0900

    [이상아] feat: Login UI implementing [FLP]

---
+++ sangaLee/frontend/src/components/LoginPage/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="color"></div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: "MainLoginForm.vue",
+  data () {
+    return {
+      id: '',
+      pw: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      const { id, pw } = this
+      this.$emit('submit', { id, pw })
+    }
+  }
+}
+</script>
+
+<style>
+  @import url("./assets/css/MainLoginStyle.css");
+</style>
--- sangaLee/frontend/src/router/index.js
+++ sangaLee/frontend/src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainLogin from '../views/LoginPage/MainLogin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'HomeView',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'MainLogin.vue',
+    component: MainLogin
   }
 ]
 
