4f2faebb90f779ee77a0566bc9f7aaa52d81d3dc
Mong9 <bean326@naver.com>
2022-04-02 22:57:14 +0900

    💄 Add ProductPage style

---
+++ SungBeanPark/frontend/src/views/Product/ProductPage.vue
@@ -0,0 +1,278 @@
+<template>
+  <body>
+    <Header-Layout></Header-Layout>
+    <aside class="sidebar">
+      <header class="sidebar-header">
+        <h1 class="logo">
+          <a href="/">
+            <img src="@/assets/images/Logo.svg" alt="나이키" />
+          </a>
+        </h1>
+
+        <nav class="sidebar-nav">
+          <h2 class="visually-hidden">메뉴</h2>
+          <div class="sidebar-user-menu">
+            <ul class="user-menu-list">
+              <li class="user-menu-item"><a href="/">Best</a></li>
+              <li class="user-menu-item"><a href="/">New Release</a></li>
+              <li class="user-menu-item"><a href="/">Men</a></li>
+              <li class="user-menu-item"><a href="/">Women</a></li>
+              <li class="user-menu-item"><a href="/">Login</a></li>
+              <li class="user-menu-item"><a href="/">Cart</a></li>
+              <li class="user-menu-item"><a href="/">Support</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </aside>
+    <section class="product-section product-recommendation">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <header class="product-section-header">
+              <h2 class="title">Shoes</h2>
+            </header>
+
+            <div class="product-section-content">
+              <ul class="product-list">
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/921826-101/039eed3d-8937-443e-b1bc-ce12b38ae95b_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">나이키 에어맥스 97</h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>4.6</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DM0013-700/f88dcb58-467e-4221-be4b-5f29d4da022c_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">나이키 에어맥스 던</h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>4.6</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DH9522-101/b5b3b412-6804-455c-b018-3f1dfff40bc4_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">나이키 에어맥스 던</h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>4.6</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DV0743-100/312379c0-8dda-4f0b-8d50-42219a9e7058_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">나이키 와플 데뷰</h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>2.2</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DM0180-600/3bbb872b-fb30-4d8e-9fc7-e6b4b41d0dd0_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">
+                        나이키 와플 트레이너 SE
+                      </h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>3.0</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+                <li class="product-item">
+                  <a href="/">
+                    <div class="product-card">
+                      <div class="product-card-image">
+                        <img
+                          src="https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DH8011-001/baf64f74-f244-45fc-8c18-76c45e0e033b_primary.jpg?browse"
+                          alt=""
+                        />
+                        <button>
+                          <i class="ic-bookmark"></i>
+                        </button>
+                      </div>
+
+                      <h1 class="product-card-title">
+                        나이키 에어 모어 업템포
+                      </h1>
+
+                      <div class="product-card-price">
+                        <span class="percent">10%</span>
+                        <strong class="price">179,100</strong>
+                        <strong class="price-off">199,000</strong>
+                      </div>
+                      <div class="product-card-subtitle">
+                        <span class="only">남성신발</span>
+                      </div>
+
+                      <dl class="product-card-detail">
+                        <div class="rating">
+                          <dt>
+                            <i class="ic-star"></i>
+                            <span class="visually-hidden">평점</span>
+                          </dt>
+                          <dd>3.0</dd>
+                        </div>
+                      </dl>
+                    </div></a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer-layout></Footer-layout>
+  </body>
+</template>
+<script>
+import HeaderLayout from "@/components/Header/HeaderLayout.vue"
+import FooterLayout from "@/components/Footer/FooterLayout.vue"
+
+export default {
+  components: { HeaderLayout, FooterLayout },
+}
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/scss/layout/product";
+@import "~@/assets/scss/layout/product-section";
+</style>
