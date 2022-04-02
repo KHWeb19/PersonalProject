e198a68f07c3b52bb1284ee552ad586eaa919550
Mong9 <bean326@naver.com>
2022-04-02 22:59:55 +0900

    🎨 Improve structure

---
+++ SungBeanPark/frontend/src/views/Account/IdPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <id-page-form></id-page-form>
+</template>
+<script>
+import IdPageForm from "@/components/Account/IdPageForm.vue"
+export default {
+  name: "IdPage",
+
+  components: { IdPageForm },
+}
+</script>
+<style></style>
---
+++ SungBeanPark/frontend/src/views/Account/LoginPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <login-page-form></login-page-form>
+</template>
+<script>
+/* eslint-disable */
+
+import LoginPageForm from "@/components/Account/LoginPageForm.vue"
+import Vue from "vue"
+import axios from "axios"
+export default {
+  name: "LoginPage",
+  components: {
+    LoginPageForm,
+  },
+  methods: {
+    onSubmit(payload) {
+      if (!this.isLogin) {
+        const { id, pw } = payload
+        axios
+          .post("http://localhost:8888/Member/login", { id, pw })
+          .then((res) => {
+            if (res.data) {
+              alert("로그인 성공!")
+              this.$store.state.userInfo = res.data
+              this.isLogin = true
+            }
+          })
+          .catch((res) => {
+            alert(res.response.data.message)
+          })
+      } else {
+        alert("이미 로그인이 되어 있습니다!")
+      }
+    },
+  },
+}
+</script>
+<style></style>
