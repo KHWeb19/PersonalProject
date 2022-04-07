48f68f0d8bf5e8ed8a8e17cd4ddd32bd3daf259a
930212@naver.com <[!rlarnrgus12]>
2022-04-07 16:49:06 +0900

    [김국현] init: repo initialization

---
+++ KukhyunKim/spring/demo/src/main/java/com/example/demo/service/order40/MemberServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.demo.service.order40;
+
+import com.example.demo.entity.order40.Member;
+import com.example.demo.repository.order40.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    MemberRepository repository;
+
+    @Override
+    public void register(Member member) {
+        repository.create(member);
+    }
+
+    @Override
+    public List<Member> list() {
+        return repository.list();
+    }
+
+    @Override
+    public Member read(Integer memberNo) {
+        return repository.read(memberNo);
+    }
+
+    @Override
+    public void modify(Member member) {
+        repository.update(member);
+    }
+
+    @Override
+    public void remove(Integer memberNo) {
+        repository.delete(memberNo);
+    }
+
+    @Override
+    public Boolean login(Member member) {
+        // 특정 id 값을 가지고 있는지 검사
+        // 해당 id 값의 pw가 일치하는지 확인
+        // 모두 OK라면 true 리턴
+        // 도중에 하나라도 문제가 발생한다면 false 리턴
+        Member findMember = repository.findMemberById(member);
+
+        if (findMember == null) {
+            System.out.printf("There are no %s id\n", member.getId());
+            return false;
+        }
+
+        System.out.println("Check Password Only: " + findMember);
+        Boolean loginSuccess = findMember.getPw().equals(member.getPw());
+
+        return loginSuccess;
+    }
+}
--- KukhyunKim/vue/frontend/src/views/Home.vue
+++ KukhyunKim/vue/frontend/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <router-link style="text-decoration: none;" :to="{ name: 'Home' }">
+       &nbsp;Home&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'VueFileUploadPage' }">
+        &nbsp;File Upload Test&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'VuetifyMemberRegisterPage' }">
+      &nbsp;Vuetify Member Register Test&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'VuetifyMemberJoinColumnTestPage' }">
+      &nbsp;Vuetify Member Join Column Test&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'LoginTestPage' }">
+      &nbsp;Login Test&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'JpaBoardListPage' }">
+      &nbsp;Jpa Board List&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'JpaMemberAuthTestPage' }">
+      &nbsp;Business Member Check&nbsp;
+    </router-link>
+    <router-link style="text-decoration: none;" :to="{ name: 'DaumNewsCrawlerPage' }">
+      &nbsp;다음 뉴스 크롤링
+    </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
+
 </script>
