86b08a6ab0bfde0f8f032957ac6dfc3b125df95e
930212@naver.com <[!rlarnrgus12]>
2022-04-09 01:08:55 +0900

    [김국현]init repo prepare board

--- KukhyunKim/vue/frontend/src/store/index.js
+++ KukhyunKim/vue/frontend/src/store/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import state from './states'
-import actions from './actions'
-import mutations from './mutations'
-import getters from './getters'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  actions,
   mutations,
-  getters
+  actions,
+  modules
 })
