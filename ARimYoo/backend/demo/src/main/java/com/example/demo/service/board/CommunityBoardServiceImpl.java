72a221e9ebb78472d306772369943cb47b58e86d
rimirim <rimizzaang@naver.com>
2022-04-11 17:58:03 +0900

    [유아림] feat : 게시판 업로드한 이미지 보이기 [B-B1]

--- ARimYoo/backend/demo/src/main/java/com/example/demo/service/board/CommunityBoardServiceImpl.java
+++ ARimYoo/backend/demo/src/main/java/com/example/demo/service/board/CommunityBoardServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -24,14 +25,15 @@ public class CommunityBoardServiceImpl implements CommunityBoardService {
 
     @Override
     public void register(CommunityBoard board, @RequestParam(required = false) MultipartFile file) throws Exception {
-        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+//        String projectPath = System.getProperty("user.dir") + "../../frontend/src/assets/back"
 
         if (file != null) {
             UUID uuid = UUID.randomUUID();
             String fileName = uuid + "_" + file.getOriginalFilename();
-            File saveFile = new File(projectPath, fileName);
+            FileOutputStream saveFile = new FileOutputStream("../../frontend/src/assets/back/" + fileName);
 
-            file.transferTo(saveFile);
+            saveFile.write(file.getBytes());
+            saveFile.close();
 
             board.setFileName(fileName);
             board.setFilePath("/files/" + fileName);
