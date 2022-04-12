e26f41d2f942ad5c6bcbd507dfc66dfd3240893a
sknyuki <sknyuki009@gmail.com>
2022-04-13 01:50:44 +0900

    [세키네]CheckBrand Image File upload(backend)[C-IF]

---
+++ sknyuki/backend/demo/src/main/java/com/example/demo/Controller/FileController.java
@@ -0,0 +1,31 @@
+package com.example.demo.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/fileUpload")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
+public class FileController {
+
+    @PostMapping("/CheckBrandBoard")
+    @ResponseBody
+    public String checkBrandFileUpload(@RequestParam(value = "fileList", required = false) List<MultipartFile> fileList,
+                                      @RequestParam("boardNo") Long boardNo, @RequestParam("id") String id) {
+        try {
+            for (MultipartFile multipartFile : fileList) {
+                FileOutputStream writer = new FileOutputStream("./images/BrandCheck/" + boardNo + "_" + id + ".jpg");
+                writer.write(multipartFile.getBytes());
+                writer.close();
+            }
+        } catch (Exception e) {
+            return "Recommend File Upload Fail";
+        }
+        return "Recommend File Upload Success";
+    }
+}
