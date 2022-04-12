bfbe335bebc7329399d16cbc5eafab41a9610314
sknyuki <sknyuki009@gmail.com>
2022-04-12 16:38:22 +0900

    [세키네] 판매게시판 viewpage[K2-F]

---
+++ sknyuki/frontend/src/views/K2ProductView/K2ProductView.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-container>
+        <header-view></header-view>
+        <br>
+        <drop-down></drop-down>
+        <div id="board" align=center>
+           
+          <h3 style="padding:30px 0px 20px 0px; font-weight:bold">판매중인 상품</h3>
+     
+
+    <div class="app">
+        <product-board-list :BrandCheckboards="BrandCheckboards"/>
+    </div>
+    </div>
+     
+
+
+    </v-container>
+</template>
+<script>
+import HeaderView from '@/components/home/headerView.vue'
+import DropDown from '@/components/KategoriePage1/DropDown.vue'
+import ProductBoardList from '@/components/ProductBoard/ProductBoardList.vue'
+//import { mapState, mapActions } from 'vuex'
+
+export default {
+    name:'K2ProductView',
+    components:{ 
+    HeaderView,
+    DropDown,
+    ProductBoardList,
+        
+    },
+/* computed: {
+        ...mapState(['BrandCheckboards'])
+    },
+    mounted () {
+        this.fetchBrandCheckboards()
+    },
+    methods: {
+        ...mapActions(['fetchBrandCheckboards'])
+    }*/
+    }
+</script>
+
+<style scoped>
+.app {
+  padding:0px 50px 100px 50px;
+}
+</style>
