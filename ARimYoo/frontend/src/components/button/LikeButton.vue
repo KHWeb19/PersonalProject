14fde49b23a23f098ffce2f8c817df9a7c4690da
rimirim <rimizzaang@naver.com>
2022-04-15 15:02:04 +0900

    [유아림] feat : 게시판 좋아요 기능 추가 [F-B1]

---
+++ ARimYoo/frontend/src/components/button/LikeButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-btn color="red darken-3" dark fab small><v-icon>mdi-thumb-up</v-icon></v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'LikeButton'
+}
+</script>
+<style scoped>
+
+</style>
