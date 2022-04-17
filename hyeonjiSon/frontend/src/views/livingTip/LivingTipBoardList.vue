2dcb7ebecdfb5f5c6ce71f03e968193ae8bd0118
HyeonJi-Son <sustka46@gmail.com>
2022-04-17 18:04:48 +0900

    [손현지] 생활정보 게시판 로그인 후 이용 가능하게 변경 [LTP]

--- hyeonjiSon/frontend/src/views/livingTip/LivingTipBoardList.vue
+++ hyeonjiSon/frontend/src/views/livingTip/LivingTipBoardList.vue
@@ -1,17 +1,32 @@
 <template>
 <div id="tipboard">
-   <v-toolbar class="my-3 pt-3" color="primary" flat>
-      <v-text-field v-model="search" label="Search" append-icon="mdi-magnify" 
+<div v-if="this.$store.state.userInfo == null">
+
+<h1>회원만 이용 가능한 게시판 입니다.</h1>
+
+</div>
+
+<br>
+
+<div v-if="this.$store.state.userInfo != null">
+   <v-row>
+         <v-text-field v-model="search" label="Search" append-icon="mdi-magnify" 
       solo hide-details></v-text-field>
       
       <v-btn @click="register">
       <v-icon>add</v-icon>
       </v-btn>
-   </v-toolbar>
+   </v-row>
 
 <v-container justify-center space-between>
    <tip-board-list :tipBoards="tipBoards"/>
 </v-container>
+
+</div>
+
+<br>
+
+
 
 </div>
 </template>
