560651054002a76b95cac551b78e1ebd8c0ce9f3
sknyuki <sknyuki009@gmail.com>
2022-04-18 21:12:49 +0900

    [세키네]QuestionBoard Back 구현 및 확인[QB-B]

---
+++ sknyuki/backend/demo/src/main/java/com/example/demo/service/QuestionBoardServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.QuestionBoard;
+import com.example.demo.repository.QuestionBoardRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class QuestionBoardServiceImpl implements QuestionBoardService {
+
+    @Autowired
+    QuestionBoardRepository repository;
+
+    @Override
+    public void register(QuestionBoard board) {
+        repository.save(board);
+    }
+
+    @Override
+    public List<QuestionBoard> list() {
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "boardNo"));
+    }
+
+    @Override
+    public QuestionBoard read(Integer boardNo) {
+        Optional<QuestionBoard> maybeReadBoard = repository.findById(Long.valueOf(boardNo));
+
+        if (maybeReadBoard.equals(Optional.empty())) {
+            log.info("Can't read board!");
+            return null;
+        }
+
+        return maybeReadBoard.get();
+    }
+
+    @Override
+    public void modify(QuestionBoard board) {
+        repository.save(board);
+    }
+
+    @Override
+    public void remove(Integer boardNo) {
+        repository.deleteById(Long.valueOf(boardNo));
+    }
+}
