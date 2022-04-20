9527b0b0ef1c33d8aec4cc306beaf2ac45a7da79
jiyeon12345 <wldys5353@naver.com>
2022-04-20 15:40:22 +0900

    [인지연] feat: MY페이지_예약 내역 내림차순으로 읽기[MYMS]

--- jiyeonIn/backend/demo/src/main/java/com/example/demo/repository/booking/BookingRepository.java
+++ jiyeonIn/backend/demo/src/main/java/com/example/demo/repository/booking/BookingRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<BookingInfo, Long> {
 
-    @Query(value = "select * from booking_info where id = :checkId",nativeQuery = true)
+    @Query(value = "select * from booking_info where id = :checkId order by booking_no desc",nativeQuery = true)
     public List<BookingInfo> findIdList(String checkId);
 }
