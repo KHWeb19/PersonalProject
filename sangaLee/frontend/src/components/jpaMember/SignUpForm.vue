8a59bf5293330a45e501c7c87c5b775fd20a4c39
asalwaysineedu <lso7182@naver.com>
2022-04-22 19:29:46 +0900

    [이상아] feat: 회원가입 UI 최종 완료 [FSP]

--- sangaLee/frontend/src/components/jpaMember/SignUpForm.vue
+++ sangaLee/frontend/src/components/jpaMember/SignUpForm.vue
@@ -1,48 +1,43 @@
 <template>
-  <section>
-      <div class="container">
-        <div class="form">
+      <div class="background">
 
           <form @submit.prevent="onSubmit">
 
-            <h2>회원가입</h2>
+            <h1>(σ≥∀⊙)σ♡</h1>
+            <br>
             <!-- 아이디 입력 -->
             <div class="inputBox">
-              <input type="text" v-model="id" placeholder="아이디를 입력해주세요.">
+              <input type="text" v-model="id" placeholder="아이디">
             </div>
-
+            <br>
             <!-- 비밀번호 입력 -->
             <div class="inputBox">
-              <input type="pw" v-model="pw" placeholder="비밀번호를 입력해주세요.">
+              <input type="password" v-model="pw" placeholder="비밀번호">
             </div>
-            
+            <br>
             <!-- 비밀번호 확인 -->
             <div class="inputBox">
-              <input type="pwcheck" placeholder="비밀번호를 한번 더 입력해주세요.">
+              <input type="pwcheck" placeholder="비밀번호 확인">
             </div>
-
+            <br>
             <!-- 이메일 -->
             <div class="inputBox">
-              <input type="email" v-model="email" placeholder="이메일 주소를 입력해주세요.">
+              <input type="email" v-model="email" placeholder="이메일">
             </div>
 
             <!-- 가입하기 버튼 -->
-            <div class="inputBox">
-            <input type="submit" value="가입하기">
+            <div class="buttonA">
+            <button type="submit" value="가입하기">가입하기</button>
             </div>
 
-            <!--
-            취소 버튼 : 로그인 페이지로 이동
-            <div class="inputBox">
-            <router-link :to="{ name: 'LoginPage' }">취소</router-link>
-            </div>
-            -->
+
+            <!-- 뒤로가기 버튼 -->
+            <router-link :to="{name: 'MainPage'}">
+              <button class="backbtn"><i class="fas fa-arrow-left"></i></button>
+            </router-link>
 
           </form>
-
-        </div>
       </div>
-  </section>
 </template>
 
 <script>
@@ -67,4 +62,81 @@ export default {
 
 </script>
 
-<style scoped src="../../assets/css/SignupStyle.css"></style>
+<style scoped>
+.background {
+  background-color: #EBECF0;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+  padding: 200px;
+}
+h1 {
+  color: #6768ab;
+}
+form {
+  width: 100%;
+  justify-content: center;
+}
+input {
+  box-shadow: 0px -4px 10px rgba(255, 255, 255, 0.2), 0px 4px 6px #e8e8f3, inset 0px -4px 12px rgba(255, 255, 255, 0.6), inset 0px -2px 2px rgba(255, 255, 255, 0.2), inset 0px 4px 6px #c6c6e2;
+  border: none;
+  padding: 1em 2.7em;
+  border-radius: 50em;
+  background: transparent;
+  color: #6768ab;
+  text-shadow: 1px 1px rgba(255, 255, 255, 0.6);
+}
+input::placeholder {
+  color: #b6b6d9;
+  text-shadow: 1px 1px rgba(255, 255, 255, 0.6);
+}
+input:focus {
+  outline: none;
+  color: #a5a5d0;
+}
+.buttonA {
+transition: box-shadow 0.3s ease, color 0.3s ease;
+display: inline-block;
+height: 48px;
+border-radius: 50em;
+line-height: 48px;
+color: #6768ab;
+letter-spacing: 0.1em;
+font-weight: bold;
+cursor: pointer;
+text-transform: uppercase;
+text-shadow: 1px 1px rgba(255, 255, 255, 0.6);
+margin: 0 2.7em;
+margin-top: 25px;
+margin-left: 30px;
+padding: 0 3.5em;
+box-shadow: -4px -4px 6px rgba(255, 255, 255, 0.7), 4px 4px 6px #c0c0de, inset -4px -4px 12px rgba(255, 255, 255, 0.1), inset 4px 4px 12px #e8e8f3;
+}
+.backbtn {
+transition: box-shadow 0.3s ease, color 0.3s ease;
+display: inline-block;
+height: 48px;
+border-radius: 50em;
+text-align: center;
+line-height: 48px;
+color: #6768ab;
+letter-spacing: 0.1em;
+font-weight: bold;
+cursor: pointer;
+text-transform: uppercase;
+text-shadow: 1px 1px rgba(255, 255, 255, 0.6);
+margin: 0 2em;
+margin-left: -10px;
+padding: 0 1.5em;
+box-shadow: -4px -4px 6px rgba(255, 255, 255, 0.7), 4px 4px 6px #c0c0de, inset -4px -4px 12px rgba(255, 255, 255, 0.1), inset 4px 4px 12px #e8e8f3;
+}
+[class*=button]:hover {
+color: #6768ab;
+}
+.buttonA:hover {
+box-shadow: -4px -4px 10px rgba(255, 255, 255, 0.2), 4px 4px 6px #e8e8f3, inset -4px -4px 12px rgba(255, 255, 255, 0.6), inset -2px -2px 2px rgba(255, 255, 255, 0.2), inset 4px 4px 6px #c6c6e2;
+}
+.backbtn:hover {
+box-shadow: -4px -4px 10px rgba(255, 255, 255, 0.2), 4px 4px 6px #e8e8f3, inset -4px -4px 12px rgba(255, 255, 255, 0.6), inset -2px -2px 2px rgba(255, 255, 255, 0.2), inset 4px 4px 6px #c6c6e2;
+}
+</style>
