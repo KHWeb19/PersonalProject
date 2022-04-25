f46471493d5fc20a7736acffd45cd8cbe7bd00d7
HyeonJi-Son <sustka46@gmail.com>
2022-04-25 21:22:39 +0900

    [손현지] 게시판 완료

--- hyeonjiSon/backend/src/main/java/com/example/demo/repository/StoreBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StoreBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreBoardRepository  extends JpaRepository<StoreBoard, Long> {
-}
