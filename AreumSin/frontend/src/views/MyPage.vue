20e9a7e84b49ddac1362681cd747bcb767ed3ec1
sinareum <kuuniin@gmail.com>
2022-04-27 22:28:49 +0900

    [신아름] MyPage [MP]

--- AreumSin/frontend/src/views/MyPage.vue
+++ AreumSin/frontend/src/views/MyPage.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    MyPage
-    <p id="setColor"> 안녕 </p>
+
   </div>
 
 
 </template>
 
 <script>
+
 export default {
   name: "MyPage",
   data(){
     return{
-      color: localStorage.getItem(localStorage.getItem('session'))
+
     }
   },
-  created() {
-    alert(this.color)
-    const element = document.getElementById('setColor');
-
-    element.style.color = this.color;
+  methods: {
   }
 }
 </script>
