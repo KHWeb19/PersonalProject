f2a9569c985ed9eaa7128e93a24b3a85804f8f7e
HyeonJi-Son <sustka46@gmail.com>
2022-04-28 21:11:28 +0900

    [손현지] 분리수거 안내 페이지 - 지도 연동 페이지 iframe 삽입 [GMP]

---
+++ hyeonjiSon/frontend/src/components/recycleGuide/GuideBoxForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <span>guide</span>
+
+        <table width="800">
+            <tr>
+                <td align="right">
+                        <v-btn>
+                            동사무소
+                        </v-btn>
+
+                        <v-btn>
+                            의류 수거함
+                        </v-btn>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+            </tr>
+        </table>
+
+    <div class="video-container">
+            <iframe width="1000" height="720" src="https://www.google.com/maps/d/embed?mid=1EdVUZBtD6W4G0QMMXKVoLU-MzmCM3WmE&ehbc=2E312F"
+                    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+            </iframe>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
