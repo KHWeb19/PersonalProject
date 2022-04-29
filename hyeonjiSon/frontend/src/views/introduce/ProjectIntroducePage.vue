172d1c900faac0f8659f5bdfcf7f041bc05e30e9
HyeonJi-Son <sustka46@gmail.com>
2022-04-29 23:52:22 +0900

    [손현지] 프로젝트 소개 페이지 [PIP]

--- hyeonjiSon/frontend/src/views/introduce/ProjectIntroducePage.vue
+++ hyeonjiSon/frontend/src/views/introduce/ProjectIntroducePage.vue
@@ -1,11 +1,31 @@
 <template>
-<div>
-   <h1> Project Introduce Page </h1>
+<div align="center">
+   <br><br>
+   <h1> Project: 3re GUIDE </h1>
+   <br><br><br>
+   <v-container>
+      <v-row justify="center">
+         <img src="@/assets/introduce/introduce.png" height="381px"/>
+         &ensp;&ensp;
+         <introduce-p/>
+      </v-row>
+   </v-container>
 </div>
 </template>
 
 <script>
+import introduceP from '@/components/introduce/introduceP.vue'
+
 export default {
-    
+    name: 'projectIntroducePage',
+    components: {
+       introduceP
+    }
 }
 </script>
+
+<style scoped>
+h1{
+  color: #907bb0;
+}
+</style>
