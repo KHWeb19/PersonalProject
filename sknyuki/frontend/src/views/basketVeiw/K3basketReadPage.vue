1b8277b4de39d64a16d46ab25ab7efab902df18d
sknyuki <sknyuki009@gmail.com>
2022-05-03 22:36:56 +0900

    [세키네]Basket Board store[BB-S]

---
+++ sknyuki/frontend/src/views/basketVeiw/K3basketReadPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+        <header-view></header-view>
+        <br>
+     <div align="center">
+        <basket-read :ProductBoardB="ProductBoardB" :boardNo=" boardNo"/>
+        
+            <br>
+
+           
+                <v-btn text color="black" rounded x-large
+                       style="padding: 10px; width: 90px;">
+                장바구니에서 삭제
+                </v-btn>
+        </div>
+    
+    </v-container>   
+</template>
+<script>
+import { mapState, mapActions } from 'vuex'
+import HeaderView from '@/components/home/headerView.vue'
+import BasketRead from '@/components/BasketBoard/BasketRead.vue'
+
+export default {
+    name:'K3basketReadPage',
+    components:{ 
+    HeaderView,
+    SoldBoardRead,
+    BasketRead,
+    },
+    props: {
+        boardNo: {
+            type: String,
+            required: true
+        }   
+    },
+    computed: {
+        ...mapState(['BasketBoard','userInfo'])
+    },
+    created () {
+            this.fetchBasketBoard(this.boardNo)
+                .catch(err => {
+                    alert(err.response.data.message)
+                    this.$router.push()
+                })
+    },
+     methods: {
+        ...mapActions(['fetchBasketBoard'])},
+}
+</script>
