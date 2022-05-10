1e5cadc53d2dd3104af482fff4a4a2a437601b18
potential007 <potential007@naver.com>
2022-05-10 20:27:25 +0900

    [이지상]메인화면시스템[mps]

--- JisangLee/frontend/src/App.vue
+++ JisangLee/frontend/src/App.vue
@@ -1,50 +1,81 @@
+<style>
+.Main{     
+  height: 20vh;
+  overflow: hidden;
+  margin:0;
+  background-color: #D3D3D3;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100vw; 
+}
+.background{
+    height: 120vh;
+    overflow: hidden;
+    margin:0;
+    background-image: url("../src/assets/img/main.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.navbar { 
+    width: 100vw; 
+    position: fixed; 
+    transform: translate3d(0, 0, 0); 
+    transition: 0.1s all ease-out; 
+    } 
+.navbar.navbar--hidden { 
+    box-shadow: none; 
+    transform: translate3d(0, -100%, 0);
+} 
+#contact{
+    width: 100%; 
+    height: 80px;
+    background-color:rgb(40, 44, 53) ;
+    position: relative;
+}
+#icon{
+    text-align: center;
+    position: relative;
+}
+</style>
 <template>
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <div class="Main" align="center">
+       <header>
+            <div class="navbar" 
+                 :class="{ 'navbar--hidden': !showNavbar }" >
+                </div> 
+                <br>
+            <a href="http://localhost:8080/stock/"><img src='../src/assets/img/newlogo.png'></a>
+            <br>
+        </header>     
+    </div>
+    <div id= article v-on:click="checkLogin">
+        <v-btn v-bind:disabled="this.$store.state.userInfo == null" class="grey darken-1 white--text" width="325px" onclick="location.href='http://localhost:8080/stock/LocalList'" :style="{fontSize:'x-large'}">국내 주식 분석</v-btn>
+        <v-btn v-bind:disabled="this.$store.state.userInfo == null" class="grey darken-2 white--text" width="325px" onclick="location.href='http://localhost:8080/stock/ForeignList'" :style="{fontSize:'x-large'}">해외 주식 분석</v-btn>
+        <v-btn v-bind:disabled="this.$store.state.userInfo == null" class="grey darken-3 white--text" width="325px" onclick="location.href='http://localhost:8080/stock/QuestionList'" :style="{fontSize:'x-large'}">질문</v-btn>
+        <v-btn v-bind:disabled="this.$store.state.userInfo == null" class="grey darken-4 white--text" width="325px" onclick="location.href='http://localhost:8080/stock/ComplaintList'" :style="{fontSize:'x-large'}">하소연</v-btn> 
+    </div>       
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
-
-    <v-main>
+    <v-main class="background" id="vm" align="center">
       <router-view/>
     </v-main>
+    <footer id=contact>             
+            <div id=icon>
+                <v-icon color="white">mdi-twitter</v-icon>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   
+                <v-icon color="white">mdi-facebook</v-icon>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    
+                <v-icon color="white">mdi-whatsapp</v-icon>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+                <v-icon color="white">mdi-wechat</v-icon>
+            </div>   
+            <p align="center" style="font-size:16px; color:white;">JulinCommu | 대표자 : 이지상 | 주소 : 서울특별시 영등포구 영등포로 3길 23 <br> 
+                                                                   Copyright©2022 CommunityForJulin. All rights reserved.</p>              
+    </footer>
   </v-app>
 </template>
 
 <script>
-
+import swal from 'sweetalert';
 export default {
 name: 'App',
  data: () => ({
@@ -59,8 +90,25 @@ name: 'App',
   window.addEventListener('beforeunload', () => {
    sessionStorage.setItem('store', JSON.stringify(this.$store.state))
   })
+ },
+ methods:{
+ checkLogin(){
+        if(this.$store.state.userInfo == null){
+             swal({
+                text: "로그인을 먼저 해주세요.",
+                icon:"warning"
+                })
+        }
+    },
+    onScroll () { 
+          const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop 
+          if (currentScrollPosition < 0) { return } // Stop executing this function if the difference between // current scroll position and last scroll position is less than some offset 
+          if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 20) {return } 
+          this.showNavbar = currentScrollPosition < this.lastScrollPosition 
+          this.lastScrollPosition = currentScrollPosition 
+        },
  }
 }
 </script>
 
- 
+
--- JisangLee/frontend/src/router/index.js
+++ JisangLee/frontend/src/router/index.js
@@ -5,7 +5,26 @@ import MainPage from '../components/main/MainPage.vue'
 
 import RegisterPage from '@/views/stockMember/RegisterPage.vue'
 import LoginPage from '@/views/stockMember/LoginPage.vue'
-import AwesomeSwiperPage from '@/views/ui/AwesomeSwiperPage.vue'
+
+import LocalBoardListPage from '@/views/stockBoard/local/LocalBoardListPage.vue'
+import LocalBoardRegisterPage from '@/views/stockBoard/local/LocalBoardRegisterPage.vue'
+import LocalBoardReadPage from '@/views/stockBoard/local/LocalBoardReadPage.vue'
+import LocalBoardModifyPage from '@/views/stockBoard/local/LocalBoardModifyPage.vue'
+
+import ForeignBoardListPage from '@/views/stockBoard/foreign/ForeignBoardListPage.vue'
+import ForeignBoardRegisterPage from '@/views/stockBoard/foreign/ForeignBoardRegisterPage.vue'
+import ForeignBoardReadPage from '@/views/stockBoard/foreign/ForeignBoardReadPage.vue'
+import ForeignBoardModifyPage from '@/views/stockBoard/foreign/ForeignBoardModifyPage.vue'
+
+import QuestionBoardListPage from '@/views/stockBoard/question/QuestionBoardListPage.vue'
+import QuestionBoardRegisterPage from '@/views/stockBoard/question/QuestionBoardRegisterPage.vue'
+import QuestionBoardReadPage from '@/views/stockBoard/question/QuestionBoardReadPage.vue'
+import QuestionBoardModifyPage from '@/views/stockBoard/question/QuestionBoardModifyPage.vue'
+
+import ComplaintBoardListPage from '@/views/stockBoard/complaint/ComplaintBoardListPage.vue'
+import ComplaintBoardRegisterPage from '@/views/stockBoard/complaint/ComplaintBoardRegisterPage.vue'
+import ComplaintBoardReadPage from '@/views/stockBoard/complaint/ComplaintBoardReadPage.vue'
+import ComplaintBoardModifyPage from '@/views/stockBoard/complaint/ComplaintBoardModifyPage.vue'
 
 Vue.use(VueRouter)
 
@@ -37,10 +56,123 @@ const routes = [
     }
   },
   {
-    path: '/awesomeSwiper',
-    name: 'AwesomeSwiperPage',
+    path: '/stock/LocalList',
+    name: 'LocalBoardListPage',
+    component: LocalBoardListPage
+  },
+  {
+    path: '/stock/LocalRegister',
+    name: 'LocalBoardRegisterPage',
+    component: LocalBoardRegisterPage
+  },
+  {
+    path: '/stock/LocalRead/:boardNo',
+    name: 'LocalBoardReadPage',
     components: {
-      default: AwesomeSwiperPage
+      default: LocalBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/LocalBoardModify/:boardNo',
+    name: 'LocalBoardModifyPage',
+    components: {
+      default: LocalBoardModifyPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/ForeignList',
+    name: 'ForeignBoardListPage',
+    component: ForeignBoardListPage
+  },
+  {
+    path: '/stock/ForeignRegister',
+    name: 'ForeignBoardRegisterPage',
+    component: ForeignBoardRegisterPage
+  },
+  {
+    path: '/stock/ForeignRead/:boardNo',
+    name: 'ForeignBoardReadPage',
+    components: {
+      default: ForeignBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/ForeignBoardModify/:boardNo',
+    name: 'ForeignBoardModifyPage',
+    components: {
+      default: ForeignBoardModifyPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/QuestionList',
+    name: 'QuestionBoardListPage',
+    component: QuestionBoardListPage
+  },
+  {
+    path: '/stock/QuestionRegister',
+    name: 'QuestionBoardRegisterPage',
+    component: QuestionBoardRegisterPage
+  },
+  {
+    path: '/stock/QuestionRead/:boardNo',
+    name: 'QuestionBoardReadPage',
+    components: {
+      default: QuestionBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/QuestionBoardModify/:boardNo',
+    name: 'QuestionBoardModifyPage',
+    components: {
+      default: QuestionBoardModifyPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/ComplaintList',
+    name: 'ComplaintBoardListPage',
+    component: ComplaintBoardListPage
+  },
+  {
+    path: '/stock/ComplaintRegister',
+    name: 'ComplaintBoardRegisterPage',
+    component: ComplaintBoardRegisterPage
+  },
+  {
+    path: '/stock/ComplaintRead/:boardNo',
+    name: 'ComplaintBoardReadPage',
+    components: {
+      default: ComplaintBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/stock/ComplaintBoardModify/:boardNo',
+    name: 'ComplaintBoardModifyPage',
+    components: {
+      default: ComplaintBoardModifyPage
+    },
+    props: {
+      default: true
     }
   }
 ]
